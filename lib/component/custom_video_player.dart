b19c423730b6d42c47720f5f4601fb8ddc89293b
JuHan <iiiamst16@gmail.com>
2023-02-27 12:52:33 +0900

    2nd commit

---
+++ lib/component/custom_video_player.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:video_player/video_player.dart';
+import 'dart:io';
+
+class CustomVideoPlayer extends StatefulWidget {
+  final XFile video;
+
+  const CustomVideoPlayer({required this.video, super.key});
+
+  @override
+  State<CustomVideoPlayer> createState() => _CustomVideoPlayerState();
+}
+
+class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
+  VideoPlayerController? videoController;
+  @override
+  void initState(){
+    super.initState();
+
+    initializeController();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'CustomVideoPlayer',
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
